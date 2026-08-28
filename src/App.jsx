c01b14af2e3e0d45436e6657b2e7fa2d31ff00c6
Matheus Costa <mcss.pb1@gmail.com>
2026-08-28 08:35:28 -0300

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
   }
 
   return (
-    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+    <div className="w-full min-h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
         <Title>Gerenciador de Tarefas</Title>
         <AddTask onAddTaskSubmit={onAddTaskSubmit}></AddTask>
